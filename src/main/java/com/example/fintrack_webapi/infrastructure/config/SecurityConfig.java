58bbc5de3c7173091068664fef884abb2ed31dd5
Lanza777 <mateo.vasquezg@udea.edu.co>
2026-05-04 21:25:12 -0500

    Agregando url frontend

--- src/main/java/com/example/fintrack_webapi/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/example/fintrack_webapi/infrastructure/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://fintrack-frontend-rho.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
         config.setAllowCredentials(true);
